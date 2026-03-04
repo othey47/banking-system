653586cc6820c0f5b6f024505be28c04c374588a
OTHMANE <129087648+othey47@users.noreply.github.com>
2026-03-04 06:41:18 +0000

    Add SavingsAccount class with interest rate and apply_interest method

--- savings.py
+++ savings.py
@@ -1,6 +1,6 @@
 
 from account import Account
-
+from storage import Storage
 
 class SavingsAccount(Account):
     """
@@ -40,6 +40,28 @@ class SavingsAccount(Account):
         """
         interest = self.get_balance() * self.__interest_rate
         self.deposit(interest)
+    
+    def to_dict(self):
+        """
+        Convert the SavingsAccount object into a dictionary.
+
+        This method is mainly used for:
+         - Serializing account data.
+         - Saving account information to JSON files.
+         - Preparing structured data output
+        
+        Returns:
+            dict: A dictionary representation of the savings account including id, type, name, balance, and interest rate.
+        """
+        return {
+            "id" : self.get_id(),                        # Unique account identifier
+            "type": "savings",                           # Account type (used for reconstruction)
+            "name": self.get_name(),                     # Account holder name
+            "balance": self.get_balance(),               # Current account balance
+            "interest_rate": self.get_interest_rate()    # Savings interest rate
+        }
+
+
     # Override __str__ for detailed account info
     def __str__(self):
         """Return formatted account info including interest rate"""
